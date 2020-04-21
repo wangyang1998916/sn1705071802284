c43e6ddc53f0be21aafb73ad7afcc036f63c820a
wangyang1998916 <2931113553@qq.com>
2020-04-21 18:20:51 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180228/MyReceiver.java
@@ -0,0 +1,18 @@
+package cn.edu.sdwu.android02.classroom.sn170507180228;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //当接到广播 自动调用 本方法
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+       String content=intent.getStringExtra("key1");
+        Toast.makeText(context,content,Toast.LENGTH_SHORT).show();
+    }
+}
